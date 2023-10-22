c8a73633d0446992903a83b78d33a312c36ac0e2
Panos Korovesis <panoskorovesis@outlook.com>
2023-10-22 20:36:35 +0300

    Add aiflow

---
+++ airflow/dags/data_ingestion_local.py
@@ -0,0 +1,49 @@
+import os
+
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from ingest_script import ingest_callable
+
+
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+
+local_workflow = DAG(
+"LocalIngestionDag_FINAL",
+    schedule_interval="@monthly",
+    start_date=datetime(2022, 1, 1),
+    end_date=datetime(2022, 11, 1),
+    # catchup=False,
+    max_active_runs=1
+)
+
+URL_PREFIX = 'https://d37ci6vzurychx.cloudfront.net/trip-data' 
+URL_TEMPLATE = URL_PREFIX + '/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/output_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+TABLE_NAME_TEMPLATE = 'yellow_taxi_{{ execution_date.strftime(\'%Y_%m\') }}'
+
+with local_workflow:
+    wget_task = BashOperator(
+        task_id='wget',
+        bash_command=f'curl -sSL {URL_TEMPLATE} > {OUTPUT_FILE_TEMPLATE}'
+    )
+
+    check_tast = BashOperator(
+        task_id='check',
+        bash_command=f'ls -a {AIRFLOW_HOME}'
+    )
+    
+    ingest_task = PythonOperator(
+        task_id="ingest",
+        python_callable=ingest_callable,
+        op_kwargs=dict(
+            table_name=TABLE_NAME_TEMPLATE,
+            csv_file=OUTPUT_FILE_TEMPLATE,
+            color='yellow'
+        ),
+    )
+
+    wget_task >> check_tast >> ingest_task
---
+++ airflow/dags/ingest_script.py
@@ -0,0 +1,78 @@
+import os
+from time import time
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+from airflow.hooks.base_hook import BaseHook
+
+import pip
+import pkg_resources
+
+def install_packages():
+       
+    installed_packages = pkg_resources.working_set
+    installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+    for i in installed_packages])
+
+    required_packages = ['psycopg2', 'pyarrow', 'pandas', 'sqlalchemy']
+
+    for package in required_packages:
+        if package not in installed_packages_list:
+            pip.main(['install', package])
+
+def ingest_callable(table_name, csv_file, execution_date, color):
+
+    install_packages()
+
+    print(table_name, csv_file, execution_date)
+
+    #the connection will be done by importing the created connection
+    airflow_conn = BaseHook.get_connection('pg_connection')
+
+    engine = create_engine(f'{airflow_conn.conn_type}://{airflow_conn.login}:{airflow_conn.password}@{airflow_conn.host}/{airflow_conn.schema}')
+    engine.connect()
+
+    print('connection established successfully, inserting data...')
+
+    csv_name = csv_file.split('/')[-1].split('.')[0] + '.csv'
+
+    t_start = time()
+    
+    df_tmp = pd.read_parquet(csv_file, engine='pyarrow')
+    df_tmp.to_csv('/opt/airflow/' + csv_name)
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df = next(df_iter)
+
+    if color == 'yellow':
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    else:
+        df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+        df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    t_end = time()
+    print('inserted the first chunk, took %.3f seconds' % (t_end - t_start))
+
+    while True: 
+        t_start = time()
+
+        try:
+            df = next(df_iter)
+        except StopIteration:
+            print("completed")
+            break
+
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+
+        t_end = time()
+
+        print('inserted another chunk, took %.3f seconds' % (t_end - t_start))
